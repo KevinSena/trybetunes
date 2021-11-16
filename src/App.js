dc1ba968e23a2e3aa3abdedda91cbb787c36e728
Kevin Sena <senakevin50@gmail.com>
2021-11-16 16:36:22 -0300

    feat: add pages components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Search from './pages/Search';
+import HomeLogin from './pages/Login';
+import AlbumId from './pages/Album';
+import FavoritesSongs from './pages/Favorites';
+import Profile from './pages/Profile';
+import ProfileEdit from './pages/ProfileEd';
+import NotFound from './pages/NotFound';
 
 class App extends React.Component {
   render() {
-    return (<p>TrybeTunes</p>);
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route path="/search" component={ Search } />
+          <Route path="/album/:id" component={ AlbumId } />
+          <Route path="/favorites" component={ FavoritesSongs } />
+          <Route exact path="/profile" component={ Profile } />
+          <Route exact path="/profile/edit" component={ ProfileEdit } />
+          <Route exact path="/" component={ HomeLogin } />
+          <Route path="/*" component={ NotFound } />
+        </Switch>
+      </BrowserRouter>
+    );
   }
 }
 
