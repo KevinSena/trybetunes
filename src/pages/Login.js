72735e0b7358ee0b2f56e6a4ed6114c8bc1528cd
Kevin Sena <senakevin50@gmail.com>
2021-11-18 23:46:55 -0300

    feat: add create user funciton

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,13 +1,99 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { createUser } from '../services/userAPI';
 
 class Login extends Component {
+  constructor() {
+    super();
+
+    this.changeHandler = this.changeHandler.bind(this);
+
+    this.state = {
+      name: '',
+      submited: false,
+    };
+  }
+
+  minLength = () => {
+    const { name } = this.state;
+    const min = 3;
+    if (name.length >= min) return false;
+    return true;
+  }
+
+  changeHandler({ target }) {
+    const { value } = target;
+    this.setState({
+      name: value,
+    });
+  }
+
   render() {
+    const { name, submited } = this.state;
+    const { history } = this.props;
     return (
-      <div data-testid="page-login">
-        <p>trybetunes</p>
+      <div className="h-screen flex flex-col m-auto" data-testid="page-login">
+        {
+          submited ? <h1>Carregando...</h1>
+            : (
+              <div
+                className="
+                  xl:w-3/5
+                  lg:w-4/5
+                  w-full
+                  mx-auto
+                  my-10
+                  flex
+                  flex-col
+                  items-center"
+              >
+                <img src="./logo.png" alt="trybe tunes" className="w-60 mb-20" />
+                <div className="w-3/4 shadow-xl p-5 sm:p-20 rounded-xl border">
+                  <input
+                    name="name"
+                    className="
+                  mb-5
+                  w-full
+                  border
+                  border-gray-400
+                  rounded
+                  p-2
+                  focus:outline-none"
+                    type="text"
+                    placeholder="Nome"
+                    data-testid="login-name-input"
+                    onChange={ this.changeHandler }
+                  />
+                  <button
+                    type="button"
+                    className="
+                    w-full
+                    bg-blue-800
+                    text-white
+                    py-2
+                    rounded"
+                    data-testid="login-submit-button"
+                    disabled={ this.minLength() }
+                    onClick={ () => {
+                      this.setState({
+                        submited: true,
+                      });
+                      createUser({ name }).then(() => history.push('/search'));
+                    } }
+                  >
+                    Enviar
+                  </button>
+                </div>
+              </div>
+            )
+        }
       </div>
     );
   }
 }
+
+Login.propTypes = {
+  history: PropTypes.array,
+}.isRequired;
 
 export default Login;
