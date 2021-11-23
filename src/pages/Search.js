fec5b8678ceebd325f5cecfbf816de2dee6c7307
Kevin Sena <senakevin50@gmail.com>
2021-11-22 23:42:13 -0300

    feat: search requisition

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
+import searchAlbumsAPI from '../services/searchAlbumsAPI';
 
 class Search extends Component {
   constructor() {
@@ -7,6 +9,9 @@ class Search extends Component {
 
     this.state = {
       search: '',
+      searched: '',
+      submited: false,
+      result: [''],
     };
   }
 
@@ -17,8 +22,20 @@ class Search extends Component {
     });
   }
 
-  render() {
+  searcher = async () => {
     const { search } = this.state;
+    const final = await searchAlbumsAPI(search);
+    this.setState({
+      searched: search,
+      search: '',
+      submited: false,
+      result: final,
+    });
+  }
+
+  render() {
+    const { search, searched, submited, result } = this.state;
+    const singerString = 'Resultado de álbuns de: ';
     return (
       <div data-testid="page-search">
         <Header />
@@ -35,9 +52,45 @@ class Search extends Component {
             type="button"
             data-testid="search-artist-button"
             disabled={ search.length < 2 }
+            onClick={ () => {
+              this.setState({
+                submited: true,
+              });
+              this.searcher();
+            } }
           >
             Pesquisar
           </button>
+        </div>
+        <div>
+          { submited && <h1>Carregando...</h1> }
+          { result.length > 0 && searched !== '' && (
+            <p>
+              {singerString}
+              {searched}
+            </p>
+          ) }
+          { result.length > 0 ? result
+            .map((
+              {
+                collectionId,
+                artworkUrl100,
+                collectionName,
+                artistName,
+              },
+              index,
+            ) => (
+              <Link
+                key={ index }
+                to={ `/album/${collectionId}` }
+                data-testid={ `link-to-album-${collectionId}` }
+              >
+                <img src={ artworkUrl100 } alt={ collectionName } />
+                <h2>{collectionName}</h2>
+                <h3>{artistName}</h3>
+              </Link>
+            ))
+            : <p>Nenhum álbum foi encontrado</p>}
         </div>
       </div>
     );
